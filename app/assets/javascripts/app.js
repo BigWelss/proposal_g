d99dc934a8901b7fb73b9b14fa9ac4d6fdd778a1
Wellington Lucas <wls_lucas@hotmail.com>
2016-07-28 16:04:10 -0300

    arrumando header

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,15 +3,18 @@ var main = function(){
         $('.menu').animate({
             right: '0px'
         }, 200);
-
-
+        $('body').animate({
+            right: '285px'
+        },200)
     });
 
     $('.icon-close').click(function(){
         $('.menu').animate({
             right: '-285px'
         }, 200);
-
+        $('body').animate({
+            right: '0px'
+        },200)
     });
 };
 
