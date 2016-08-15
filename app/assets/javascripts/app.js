61b9c2a143d3edabfd73cb05507ce6963eb8c30b
Wellington Lucas <wls_lucas@hotmail.com>
2016-08-15 13:35:27 -0300

    arrumando js em produção

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         }, 200);
         $('body').animate({
             right: '285px'
-        },200)
+        }, 200)
     });
 
     $('.icon-close').click(function(){
@@ -14,6 +14,6 @@ $(document).ready(function(){
         }, 200);
         $('body').animate({
             right: '0px'
-        },200)
+        }, 200)
     });
 });
