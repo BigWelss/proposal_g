d8f2888442688fa241fd096b447a0fa8a32c9209
Wellington Lucas <wls_lucas@hotmail.com>
2016-08-19 17:09:14 -0300

    sombra no main aplicada

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,11 +3,13 @@ $(document).ready(function(){
         $('.menu').animate({
             right: '0px'
         }, 200);
+        $('#foreground-div').addClass('shadow');
     });
 
     $('.icon-close').click(function(){
         $('.menu').animate({
             right: '-285px'
         }, 200);
+        $('#foreground-div').removeClass('shadow');
     });
 });
